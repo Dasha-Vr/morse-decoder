e27d0c1f77ae54e6e148705ad6117f2d927c1787
Dasha-Vr <dasha-sheludyakova@yandex.ru>
2023-01-18 19:19:06 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,57 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let list = []
+    let letter = ''
+    let i = 0
+    let simbol = ''
+    while (i < expr.length){
+        while (letter.length < 10) {
+            letter = `${letter}${expr[i]}`
+            i += 1 
+        }
+        list.push(letter)
+        letter = ''
+    
+    
+    }
+    
+    let ans = []
+    for (let j = 0; j < list.length; j++) {
+    let a = ''
+        for (let i = 0; i < list[j].length; i++) {
+            if (list[j][i] == 1) {
+                if (list[j][i+1] == 1) {
+                a += '-';
+            }   else if (list[j][i+1] == 0) {
+                a += '.'
+                
+            }
+            i++
+        }
+        
+    }
+    ans.push(a)
+    }
+    
+    ans2 = []
+    for (i of ans){
+        b = MORSE_TABLE[i]
+        ans2.push(b)
+    }
+    let ans3 = []
+    
+    for (i of ans2){
+        if (i === undefined){
+            ans3.push(' ')
+        }
+        else{
+        ans3.push(i)
+            
+        }
+    }
+    
+    return ans3.join('')
 }
 
 module.exports = {
